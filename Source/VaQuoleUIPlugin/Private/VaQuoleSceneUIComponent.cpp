8b661485b1044072583a931c42e357fa000736d9
Vladimir Alyamkin <ufna@ufna.ru>
2014-10-18 18:41:46 +0400

    UE 4.5 build fix

--- Source/VaQuoleUIPlugin/Private/VaQuoleSceneUIComponent.cpp
+++ Source/VaQuoleUIPlugin/Private/VaQuoleSceneUIComponent.cpp
@@ -80,7 +80,7 @@ bool UVaQuoleSceneUIComponent::MouseMoveFromHitResult(const FHitResult& HitResul
 	UStaticMeshComponent* TargetMesh = Owner->FindComponentByClass<UStaticMeshComponent>();
 	if (TargetMesh != NULL)
 	{
-		FTransform ToActor = HitResult.Actor->GetTransform().InverseSafe();
+		FTransform ToActor = HitResult.Actor->GetTransform().Inverse();
 		FVector ActorImpact = ToActor.TransformPosition(HitResult.ImpactPoint);
 
 		FVector Min, Max;
